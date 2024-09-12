b72afc7369f754370840e1e17e12f17f0bb5f40f
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-12 14:24:31 -0500

    first commit

---
+++ src/main/java/co/edu/javeriana/msc/turismo/notification/NotificationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/msc/turismo/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
