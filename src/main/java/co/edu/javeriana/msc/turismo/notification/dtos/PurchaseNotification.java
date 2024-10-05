f33112d503d4f088de750a7df5511ca52b14efac
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-10-04 23:50:33 -0500

    se envía una notificación al correo del cliente con la confirmación de la orden

---
+++ src/main/java/co/edu/javeriana/msc/turismo/notification/dtos/PurchaseNotification.java
@@ -0,0 +1,22 @@
+package co.edu.javeriana.msc.turismo.order_management_microservice.queue.dtos;
+
+import co.edu.javeriana.msc.turismo.order_management_microservice.dto.Customer;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.enums.PaymentStatus;
+import co.edu.javeriana.msc.turismo.order_management_microservice.orders.enums.Status;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public record PurchaseNotification(
+        String id,
+        LocalDateTime creationDate,
+        LocalDateTime lastUpdate,
+        Customer purchaser,
+        Status orderStatus,
+        PaymentStatus paymentStatus,
+        List<PurchaseItem> purchaseItems,
+        Double amount
+) implements Serializable {
+}
