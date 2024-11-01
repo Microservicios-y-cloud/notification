41b82dacf777404211a6c835d3017e764c9db5a6
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-10-31 22:24:01 -0500

    corrección y verificación de excepciones y envío de códigos

---
+++ src/main/java/co/edu/javeriana/msc/turismo/notification/advice/GlobalControllerExceptionHandler.java
@@ -0,0 +1,46 @@
+package co.edu.javeriana.msc.turismo.order_management_microservice.advice;
+
+import jakarta.ws.rs.ProcessingException;
+import jakarta.ws.rs.ServiceUnavailableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleNotFound(EntityNotFoundException e) {
+        log.error("Entity not found: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handleBadRequest(IllegalArgumentException e) {
+        log.error("Bad request: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    @ExceptionHandler(ServiceUnavailableException.class)
+    public String handleServiceUnavailable(ServiceUnavailableException e) {
+        log.error("Service unavailable: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(ProcessingException.class)
+    public String handleProcessingException(ProcessingException e) {
+        log.error("Error processing message: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+
+
+}
